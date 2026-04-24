5311333685cb840c41ab819415c6382c6e80bd5f
BIGDUKE004 <elijahmiracle2007@gmail.com>
2026-04-24 07:39:21 +0100

    chin chin

---
+++ assignment/CHIN CHIN/perimeter.py
@@ -0,0 +1,9 @@
+
+edgeone = float(input("Enter first edge: "))
+edgetwo = float(input("Enter second edge: "))
+
+if edgeone != edgeone:
+    perimeter = 2 * (edge1 + edge2)
+    print("Perimeter of the rectangle:", perimeter)
+else:
+    print("Invalid input: edges must be different lengths.")
