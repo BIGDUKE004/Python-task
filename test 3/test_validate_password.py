c300d3d0ad747fa87653b55618537f7a949653cb
BIGDUKE004 <elijahmiracle2007@gmail.com>
2026-05-17 17:45:32 +0100

    Add files via upload

---
+++ test 3/test_validate_password.py
@@ -0,0 +1,11 @@
+import unittest
+import validate_password
+class validate_password(unittest.TestCase):
+	def test_test_password_validation_works(self):
+		#validate_password.password(qwertt)
+def  test_test_password_get_validated(self):
+		actual = validate_password.password(wertt)
+		expected = weak password
+		self.assertEqual(actual, expected)
+	
+
---
+++ test 3/validate_password.py
@@ -0,0 +1,5 @@
+def validate_password(password):
+    if password != 8:
+        print( weak password )
+        return password
+    print(password(wrew))
